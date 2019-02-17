a3abdc3917451466d95218ae87b4360af7134d62
rashmi2691 <rash.luck10@gmail.com>
2019-02-17 06:11:30 +0530

    First Commit

---
+++ Java/src/main/java/org/myprog/Java/App.java
@@ -0,0 +1,14 @@
+package org.myprog.Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Java");
+    }
+}
